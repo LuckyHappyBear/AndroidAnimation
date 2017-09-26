7ea0cab47298644d52fee8a574911403478fae97
LuckyHappyBear <786575831@qq.com>
2017-09-26 21:02:24 +0800

    value animation

--- app/src/main/java/com/luckybear/androidanimation/MainActivity.java
+++ app/src/main/java/com/luckybear/androidanimation/MainActivity.java
@@ -1,5 +1,6 @@
 package com.luckybear.androidanimation;
 
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ValueAnimator anim = ValueAnimator.ofInt(0, 300);
+        anim.setDuration(300);
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                int currentValue = (int) animation.getAnimatedValue();
+                android.util.Log.d("TAG", "current value is " + currentValue);
+            }
+        });
+        anim.start();
     }
 }
