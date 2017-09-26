180aaf5e2cbcbe57cc9cf94822f3508a92d13873
LuckyHappyBear <786575831@qq.com>
2017-09-26 21:25:26 +0800

    complete frame animation demo

--- app/src/main/java/com/luckybear/androidanimation/MainActivity.java
+++ app/src/main/java/com/luckybear/androidanimation/MainActivity.java
@@ -1,7 +1,9 @@
 package com.luckybear.androidanimation;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.iv_frame);
+        AnimationDrawable animationDrawable = (AnimationDrawable) getDrawable(R.drawable.frame_list);
+        imageView.setBackground(animationDrawable);
+        animationDrawable.start();
     }
 }
